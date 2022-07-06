f93acae5f2a814f95e1f4e0013e27152a30161f5
Vladimir_Kirichenko <frozenfish@internet.ru>
2022-07-06 11:42:52 +0300

    task_1.py - добавлено функция поиска с помощью bisect

--- task_1.py
+++ task_1.py
@@ -5,6 +5,8 @@
 Найти индекс первого нуля (то есть найти такое место,
 где заканчиваются единицы, и начинаются нули)
 """
+import time
+from bisect import bisect_right
 
 
 def task(array: list) -> int:
@@ -14,8 +16,39 @@ def task(array: list) -> int:
         return -1
 
 
+def task_bisect(array: list) -> int:
+    len_arr = len(array)
+    if len_arr == 0:
+        return -1
+
+    index_0 = len_arr - bisect_right(array[::-1], 0)
+    return -1 if len_arr == index_0 else index_0
+
+
 if __name__ == '__main__':
     print(task([0, 0, 0, 0, 0, 0, 0]))
     print(task([1, 1, 1, 1, 0, 0, 0]))
     print(task([1, 1, 1, 1, 1, 1, 1]))
     print(task([]))
+
+    print()
+
+    print(task_bisect([0, 0, 0, 0, 0, 0, 0]))
+    print(task_bisect([1, 1, 1, 1, 0, 0, 0]))
+    print(task_bisect([1, 1, 1, 1, 1, 1, 1]))
+    print(task_bisect([]))
+
+    print()
+
+    # поиск в большом массиве
+    array = [1] * 100000000 + [0] * 10
+
+    start_func = time.perf_counter()
+    print(task(array))
+    stop_func = time.perf_counter()
+    print(f'Время выполнения поиска index: {round(stop_func - start_func, 5)}')
+
+    start_func = time.perf_counter()
+    print(task_bisect(array))
+    stop_func = time.perf_counter()
+    print(f'Время выполнения поиска bisect: {round(stop_func - start_func, 5)}')
